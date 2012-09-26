28c06a7a8716c63d54dd62245aa15a116e41b406
Tommi Virtanen <tv@eagain.net>
2012-09-26 09:43:19 -0700

    Add "botosh -c CMD", which works a lot like "python -c CMD".

--- botosh.py
+++ botosh.py
@@ -4,6 +4,7 @@ import code
 import os
 import readline
 import rlcompleter
+import sys
 import yaml
 
 
@@ -19,6 +20,12 @@ def main():
         '--config',
         default=os.path.expanduser('~/.botosh.yaml'),
         help='Config file to read (default %(default)s)',
+        )
+    parser.add_argument(
+        '-c',
+        dest='cmd',
+        nargs='+',
+        help='program passed in as string',
         )
     args = parser.parse_args()
 
@@ -49,13 +56,23 @@ def main():
         conn = boto.s3.connection.S3Connection(**conn_kwargs)
         setattr(s3, k, conn)
 
-    readline.parse_and_bind('tab: complete')
-    code.interact(
-        banner=('Botosh: use s3 to access the Boto connections, '
-                + 'press control-D to exit...'),
-        local=dict(
-            s3=s3,
-            args=args,
-            config=config,
-            ),
+    local = dict(
+        s3=s3,
+        args=args,
+        config=config,
         )
+
+    if args.cmd is not None:
+        # argparse doesn't let us emulate python -c's "stop parsing
+        # here" style very well, so use nargs='+' and shuffle the args
+        # back to sys.argv here
+        sys.argv[:] = ['-c'] + args.cmd[1:]
+        f = compile(args.cmd[0], '<string>', 'exec')
+        exec f in local
+    else:
+        readline.parse_and_bind('tab: complete')
+        code.interact(
+            banner=('Botosh: use s3 to access the Boto connections, '
+                    + 'press control-D to exit...'),
+            local=local,
+            )
